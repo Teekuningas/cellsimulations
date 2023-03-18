e90f7526240ed700cc39566f3694f1d9ffe78ee6
Erkka Heinilä <erkkahe@gmail.com>
2023-03-18 10:45:47 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let 
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [ 
+              (pkgs.python39.withPackages (ps: [ps.matplotlib]))
+            ];
+          };
+        }
+     );
+}
