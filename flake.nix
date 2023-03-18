d72e9eac52453e02c615c4d72998932328bd5986
Erkka Heinilä <erkkahe@gmail.com>
2023-03-18 10:48:19 +0200

    Format

--- flake.nix
+++ flake.nix
@@ -3,19 +3,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let 
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          formatter = pkgs.nixfmt;
-          devShells.default = pkgs.mkShell {
-            packages = [ 
-              (pkgs.python39.withPackages (ps: [ps.matplotlib]))
-            ];
-          };
-        }
-     );
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        formatter = pkgs.nixfmt;
+        devShells.default = pkgs.mkShell {
+          packages = [ (pkgs.python39.withPackages (ps: [ ps.matplotlib ])) ];
+        };
+      });
 }
