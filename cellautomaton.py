bd999a466e1e8ec818192971017556d2d62e2d41
Erkka Heinilä <erkkahe@gmail.com>
2023-03-19 19:53:16 +0200

    Add simple plot

--- cellautomaton.py
+++ cellautomaton.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 from enum import Enum
 
@@ -67,6 +68,40 @@ class Rule1(Rule1D):
     ]
 
 
+class Rule18(Rule1D):
+    """Implementation of Rule 18."""
+
+    NAME = "Rule 18"
+
+    PATTERNS = [
+        ([Cell.ALIVE, Cell.ALIVE, Cell.ALIVE], Cell.DEAD),
+        ([Cell.ALIVE, Cell.ALIVE, Cell.DEAD], Cell.DEAD),
+        ([Cell.ALIVE, Cell.DEAD, Cell.ALIVE], Cell.DEAD),
+        ([Cell.ALIVE, Cell.DEAD, Cell.DEAD], Cell.ALIVE),
+        ([Cell.DEAD, Cell.ALIVE, Cell.ALIVE], Cell.DEAD),
+        ([Cell.DEAD, Cell.ALIVE, Cell.DEAD], Cell.DEAD),
+        ([Cell.DEAD, Cell.DEAD, Cell.ALIVE], Cell.ALIVE),
+        ([Cell.DEAD, Cell.DEAD, Cell.DEAD], Cell.DEAD),
+    ]
+
+
+class Rule105(Rule1D):
+    """Implementation of Rule 105."""
+
+    NAME = "Rule 105"
+
+    PATTERNS = [
+        ([Cell.ALIVE, Cell.ALIVE, Cell.ALIVE], Cell.DEAD),
+        ([Cell.ALIVE, Cell.ALIVE, Cell.DEAD], Cell.ALIVE),
+        ([Cell.ALIVE, Cell.DEAD, Cell.ALIVE], Cell.ALIVE),
+        ([Cell.ALIVE, Cell.DEAD, Cell.DEAD], Cell.DEAD),
+        ([Cell.DEAD, Cell.ALIVE, Cell.ALIVE], Cell.ALIVE),
+        ([Cell.DEAD, Cell.ALIVE, Cell.DEAD], Cell.DEAD),
+        ([Cell.DEAD, Cell.DEAD, Cell.ALIVE], Cell.DEAD),
+        ([Cell.DEAD, Cell.DEAD, Cell.DEAD], Cell.ALIVE),
+    ]
+
+
 def stringify_state(state):
     """Makes a string representation of a state."""
     res = ""
@@ -87,15 +122,104 @@ def stringify_history(generations):
     return res
 
 
+def plot_history(generations, rule):
+    """Makes matplotlib plot of all generations."""
+    fig, ax = plt.subplots()
+    plt.axis("off")
+    ax.set_xlim([0 - 0.5, len(generations[0].data) + 0.5])
+    ax.set_ylim([0 - 0.5, len(generations) + 0.5 + 4])
+    ax.set_title(rule.NAME)
+
+    # plot the rule patterns
+    for pattern_idx, pattern in enumerate(rule.PATTERNS):
+        # grouping box
+        group_x = 4 * pattern_idx
+        group_y = len(generations) + 4 - 3
+        rect = patches.Rectangle(
+            (group_x, group_y), 4, 3, linewidth=1, edgecolor="black", facecolor="none"
+        )
+        ax.add_patch(rect)
+
+        # and the small ones
+
+        facecolor = "black" if pattern[0][0] == Cell.ALIVE else "white"
+        rect = patches.Rectangle(
+            (group_x + 0.5, group_y + 1.5),
+            1,
+            1,
+            linewidth=1,
+            edgecolor="green",
+            facecolor=facecolor,
+        )
+        ax.add_patch(rect)
+
+        facecolor = "black" if pattern[0][1] == Cell.ALIVE else "white"
+        rect = patches.Rectangle(
+            (group_x + 1.5, group_y + 1.5),
+            1,
+            1,
+            linewidth=1,
+            edgecolor="green",
+            facecolor=facecolor,
+        )
+        ax.add_patch(rect)
+
+        facecolor = "black" if pattern[0][2] == Cell.ALIVE else "white"
+        rect = patches.Rectangle(
+            (group_x + 2.5, group_y + 1.5),
+            1,
+            1,
+            linewidth=1,
+            edgecolor="green",
+            facecolor=facecolor,
+        )
+        ax.add_patch(rect)
+
+        facecolor = "black" if pattern[1] == Cell.ALIVE else "white"
+        rect = patches.Rectangle(
+            (group_x + 1.5, group_y + 0.5),
+            1,
+            1,
+            linewidth=1,
+            edgecolor="green",
+            facecolor=facecolor,
+        )
+        ax.add_patch(rect)
+
+    # plot the generations
+    for gen_idx in range(len(generations)):
+        for cell_idx, cell in enumerate(generations[gen_idx].data):
+            if cell == Cell.ALIVE:
+                facecolor = "black"
+            else:
+                facecolor = "white"
+
+            rect = patches.Rectangle(
+                (cell_idx, len(generations) - gen_idx - 1),
+                1,
+                1,
+                linewidth=1,
+                edgecolor="green",
+                facecolor=facecolor,
+            )
+            ax.add_patch(rect)
+    plt.show()
+
+
 if __name__ == "__main__":
     """Run as a script."""
 
-    n_generations = 10
+    n_generations = 50
+    n_cells = 101
 
-    rule = Rule1()
+    rule = Rule105()
 
     # select initial state
-    state = State1D(5 * [Cell.DEAD] + [Cell.ALIVE] + 5 * [Cell.DEAD])
+    state = State1D(
+        int((n_cells - 1) / 2) * [Cell.DEAD]
+        + [Cell.ALIVE]
+        + int((n_cells - 1) / 2) * [Cell.DEAD]
+    )
 
     # Compute n_generations generations
     generations = [state]
@@ -106,4 +230,4 @@ if __name__ == "__main__":
     print("Hello automaton.")
 
     print("History of " + str(rule.NAME) + ": ")
-    print(stringify_history(generations))
+    plot_history(generations, rule)
